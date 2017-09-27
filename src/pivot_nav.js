e85c79576becd192447fb6e0d9240066a09c8272
Tweega <cpr@tweega.com>
2017-09-27 18:27:54 +0100

    added pivot_nav.js to commit

---
+++ src/pivot_nav.js
@@ -0,0 +1,43 @@
+var utils = require('./Utils');
+
+function colores(n) {
+  var cols = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
+  return cols[n % cols.length];
+}
+
+function render(nav_svg, margins, num_levels, callback) {
+  //var a = new Array(num_levels - 1);
+  var a = [...Array(num_levels - 1).keys()];
+  console.log("do we get here kjhsdfkjhkj?");
+  console.log(a.length);
+  var margin = margins.margin;
+
+  let pivot_width = 200;
+  let pivot_left = (margins.width / 2) - (pivot_width / 2);
+
+
+  nav_svg.attr("transform", "translate(" + pivot_left + "," + -10 + ")");
+  nav_svg.selectAll(".nav_button")
+    .data(a)
+    .enter()
+    .append("rect")
+      .attr("x", function(d, i) {
+        console.log("do we get here?");
+        return i * 40;
+
+      })
+      .attr("y", 0)
+      .attr("width", 10)
+      .attr("height", 10)
+      .attr("class", "pivot_nav")
+      .attr("fill", function(d,i) { return colores(i); } )
+      .on("click", click);
+
+      function click(d) {
+        console.log(d);
+        callback(d, utils.consts.PIVOT);
+      }
+  }
+
+
+exports.render = render;
